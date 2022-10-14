fedfa3e990a55c3071f07fb248accfb96f1c6588
Domenico <menico.c90@gmail.com>
2022-10-14 09:49:08 +0200

    feat: Start

---
+++ src/main/java/projectSpringBoot/projectTeam3SpringBoot/ProjectTeam3SpringBootApplication.java
@@ -0,0 +1,13 @@
+package projectSpringBoot.projectTeam3SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTeam3SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTeam3SpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projectSpringBoot/projectTeam3SpringBoot/ProjectTeam3SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package projectSpringBoot.projectTeam3SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTeam3SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
