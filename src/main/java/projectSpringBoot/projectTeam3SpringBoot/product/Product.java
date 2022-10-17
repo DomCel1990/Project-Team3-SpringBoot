767a0c4248f2de52d0f0b7bf0fd1ab27d550250a
Emilio-Fagiani <110248283+Emilio-Fagiani@users.noreply.github.com>
2022-10-17 10:05:04 +0200

    Update Product.java

--- src/main/java/projectSpringBoot/projectTeam3SpringBoot/product/Product.java
+++ src/main/java/projectSpringBoot/projectTeam3SpringBoot/product/Product.java
@@ -2,7 +2,9 @@ package projectSpringBoot.projectTeam3SpringBoot.product;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class Product {
