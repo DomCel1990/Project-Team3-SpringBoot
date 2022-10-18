2964c4c6fa610bbcaf2b7cab2d248566fbec05cc
Emilio <emi00@gmail.com>
2022-10-18 11:09:01 +0200

    feat:Created a ProductRepository

--- src/main/java/projectSpringBoot/projectTeam3SpringBoot/repositories/EmployeeRepository.java
+++ src/main/java/projectSpringBoot/projectTeam3SpringBoot/repositories/EmployeeRepository.java
@@ -1,4 +1,4 @@
-package projectSpringBoot.projectTeam3SpringBoot.repositorys;
+package projectSpringBoot.projectTeam3SpringBoot.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import projectSpringBoot.projectTeam3SpringBoot.entitis.Employee;
---
+++ src/main/java/projectSpringBoot/projectTeam3SpringBoot/repositories/ProductRepository.java
@@ -0,0 +1,7 @@
+package projectSpringBoot.projectTeam3SpringBoot.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import projectSpringBoot.projectTeam3SpringBoot.entitis.Product;
+
+public interface ProductRepository extends JpaRepository<Product,Long> {
+}
