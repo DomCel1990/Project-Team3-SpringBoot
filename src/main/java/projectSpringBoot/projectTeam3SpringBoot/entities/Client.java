ffde4493b580365106ca256fb49ea1782fe14140
Domenico <menico.c90@gmail.com>
2022-10-18 22:14:29 +0200

    feat: Fix run Applicatio

--- src/main/java/projectSpringBoot/projectTeam3SpringBoot/entities/Client.java
+++ src/main/java/projectSpringBoot/projectTeam3SpringBoot/entities/Client.java
@@ -15,7 +15,7 @@ public class Client {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
+    private Long id;
     private String name;
     private String surname;
     @Column(unique = true)
--- src/main/java/projectSpringBoot/projectTeam3SpringBoot/entities/Employee.java
+++ src/main/java/projectSpringBoot/projectTeam3SpringBoot/entities/Employee.java
@@ -16,7 +16,7 @@ public class Employee {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    private Long id;
     private String name;
     private String surname;
     @Column(unique = true)
