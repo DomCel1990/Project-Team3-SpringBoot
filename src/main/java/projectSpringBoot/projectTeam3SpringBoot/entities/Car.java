a440e78cb5de7fe744bf9e1ba13eca9260a23c34
Domenico <menico.c90@gmail.com>
2022-10-20 10:37:10 +0200

    feat: add Entity Car

---
+++ src/main/java/projectSpringBoot/projectTeam3SpringBoot/entities/Car.java
@@ -0,0 +1,45 @@
+package projectSpringBoot.projectTeam3SpringBoot.entities;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private LocalDateTime checkIn;
+    private LocalDateTime checkOut;
+
+    public Car() {}
+
+    public Car(LocalDateTime checkIn, LocalDateTime checkOut) {
+        this.setCheckIn(checkIn);
+        this.setCheckOut(checkOut);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCheckIn() {
+        return checkIn;
+    }
+
+    public void setCheckIn(LocalDateTime checkIn) {
+        this.checkIn = checkIn;
+    }
+
+    public LocalDateTime getCheckOut() {
+        return checkOut;
+    }
+
+    public void setCheckOut(LocalDateTime checkOut) {
+        this.checkOut = checkOut;
+    }
+}
