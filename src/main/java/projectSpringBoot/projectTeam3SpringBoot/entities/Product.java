ade26ef3a2e28ffebf07486fedef8f57a1d59530
Emilio <emi00@gmail.com>
2022-10-28 10:27:41 +0200

    feat: Modify product entity, added constructor and get and set methods

--- src/main/java/projectSpringBoot/projectTeam3SpringBoot/entities/Product.java
+++ src/main/java/projectSpringBoot/projectTeam3SpringBoot/entities/Product.java
@@ -1,14 +1,7 @@
 package projectSpringBoot.projectTeam3SpringBoot.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import javax.persistence.*;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
 @Entity
 @Table
 public class Product {
@@ -21,5 +14,60 @@ public class Product {
     private String type;
     private String serialCode;
 
+    public Product(Long id, String description, double price, double salePrice, String type, String serialCode) {
+        this.id = id;
+        this.description = description;
+        this.price = price;
+        this.salePrice = salePrice;
+        this.type = type;
+        this.serialCode = serialCode;
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getSalePrice() {
+        return salePrice;
+    }
+
+    public void setSalePrice(double salePrice) {
+        this.salePrice = salePrice;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSerialCode() {
+        return serialCode;
+    }
+
+    public void setSerialCode(String serialCode) {
+        this.serialCode = serialCode;
+    }
 }
